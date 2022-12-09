b50de1944a96b47651b0af607923dfab26f3525e
cocobutte <cocobutter128@gmail.com>
2022-12-09 18:07:54 +0500

    first commit

---
+++ product/src/App.jsx
@@ -0,0 +1,392 @@
+import './App.css';
+import { useState } from 'react';
+import axios from "axios"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Modal from 'react-bootstrap/Modal';
+
+
+
+
+
+let baseUrl = ""
+if (window.location.href.split(":")[0] === "http") {
+  baseUrl = "http://localhost:3000";
+  
+}
+
+
+function App() {
+
+  const [data,setData] =useState ("") 
+  const [allData,setAllData] =useState ([]) 
+  const [show, setShow] = useState(false);
+  const [editName,setEditName] =useState ("") 
+  const [editPrice,setEditPrice] =useState ("") 
+  const [editDesc,setEditDesc] =useState ("") 
+  const [editId,setEditId] =useState (null) 
+  const [editedName,setEditedName] =useState ("") 
+  const [editedPrice,setEditedPrice] =useState ("") 
+  const [editedDesc,setEditedDesc] =useState ("") 
+
+
+  
+
+
+
+
+
+  const submitHandler =(event) =>{
+    event.preventDefault()
+    let productName = event.target.productName.value
+    let productPrice = event.target.price.value
+    let productDesc = event.target.description.value
+
+    axios.post(`${baseUrl}/product`, {
+      names: productName,
+      price: productPrice,
+      description:productDesc
+    })
+    .then((response) => {
+      console.log(response);
+      setData(response.data.data)
+     
+    }, (error) => {
+      console.log(error);
+    });
+
+
+ 
+    
+
+
+  }
+
+  const alertHandler = () =>{
+    let alerta = document.getElementById("alert")
+    alerta.style.display="block"
+
+
+
+  }
+
+  const successHandler = () =>{
+    let alerta = document.getElementById("alert")
+    alerta.style.display="none"
+
+
+
+  }
+
+  const allProductsHandler=()=>{
+    axios.get(`${baseUrl}/products`)
+    .then((response) => {
+      console.log(response);
+      setAllData(response.data.data)
+     
+    }, (error) => {
+      console.log(error);
+    });
+
+        let displayDiv = document.getElementById("display")
+        displayDiv.style.display = "block"
+        
+        let addBtn = document.getElementById("add")
+        addBtn.style.display = "block"
+
+        let inputDiv = document.getElementById("input")
+        inputDiv.style.display = "none" 
+
+
+  }
+
+
+  const addShow = () =>{
+    let inputDiv = document.getElementById("input")
+    inputDiv.style.display = "block" 
+
+    let displayDiv = document.getElementById("display")
+    displayDiv.style.display = "none"
+
+    let addBtn = document.getElementById("add")
+    addBtn.style.display = "none"
+
+
+  }
+
+  const deleteProductHandler = (ids) =>{
+    console.log(ids)
+    axios.delete(`${baseUrl}/product/${ids}`)
+    .then(response => {
+      console.log("response: ", response);
+      alert("Product Deleted Successfully")
+      window.location.reload();
+
+    })
+
+    .catch(err => {
+        console.log("error: ", err);
+    })
+
+  }
+
+  const handleData = async (id,names,price,desc) =>{
+    
+      
+  
+    setShow(true)
+
+    // setEditId(id)
+    // setEditName(names)
+    // setEditPrice(price)
+    // setEditDesc(desc)
+
+    // console.log(editId)
+    // console.log(editName)
+
+
+
+  
+ 
+
+  }
+
+  const updateProductHandler = (event) =>{
+    setShow(false)
+    event.preventDefault()
+    let productName = event.target.product.value
+    let productPrice = event.target.price.value
+    let productDesc = event.target.description.value
+
+
+      axios.put(`${baseUrl}/product/${editId}`,{
+      names:productName ,
+      price: productPrice  ,
+      description: productDesc
+
+    })
+    .then((response) => {
+      console.log(response);
+     
+    }, (error) => {
+      console.log(error);
+    });
+
+    
+    alert("Your Product got updated check it out.")
+
+
+
+  }
+
+  
+
+
+
+  return (
+    <div className='main-div'>
+      <div>
+
+      </div>
+
+      <div className='add' id='add'>
+        <button onClick={addShow}>Add Product</button>
+
+      </div>
+       <div className='alert-div' id='alert'>
+        <div className='show-alert'>
+          <Alert variant="success">
+            <Alert.Heading>Hurrah!</Alert.Heading>
+            <p>
+              You have successfully added the product. Your product details are: <br /> <br />
+              Name: {data?.names} <br />
+              Price: {data?.price} <br />
+              Description: {data?.description}
+        
+            </p>
+            <hr />
+            <div className="d-flex justify-content-end">
+              <Button onClick={successHandler} variant="outline-success">
+                Ok.
+              </Button>
+            </div>
+          </Alert>
+        </div>
+
+    
+      </div>
+     
+
+      <div className='input-div' id='input'>
+        <form onSubmit={submitHandler}>
+          <input type="text" name="productName" id="name" placeholder='Enter Product Name '  /> <br />
+
+          <input type="text" name="price" id="price" placeholder='Enter Product Price'  /> <br />
+
+          <textarea name="description" id="" cols="80" rows="5" placeholder='Enter Product Description...'></textarea>
+
+          <Button variant="outline-success" type='submit' className='btn' onClick={alertHandler}>Add Product</Button>
+          <Button variant="outline-info" className='btn' onClick={allProductsHandler}>Get All Products</Button>{' '}
+
+
+        </form>
+      </div>
+
+
+        
+
+
+
+
+
+      <div className='display-div' id='display'>
+
+
+        {
+
+              (allData.length !== 0)?
+                allData.map((eachData,i) => (
+                  <div key={i}>
+
+                  
+                    <Table  striped bordered hover variant="dark">
+                      <thead>
+                        <tr>
+                          <th>#ID</th>
+                          <th>Product Name</th>
+                          <th>Product Price</th>
+                          <th>Description</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>{eachData?.id}</td>
+                          <td>{eachData?.names}</td>
+                          <td>{eachData?.price}</td>
+                          <td>{eachData?.description}
+                          <button onClick={()=>{
+                            deleteProductHandler(eachData?.id)
+
+                          }}>Delete</button>
+
+                          <button onClick={()=>{
+                            // handleData(eachData.id,eachData?.names,eachData?.price,eachData?.description)
+                          handleData(
+                            setEditId(eachData.id),
+                            setEditName(eachData?.names),
+                            setEditPrice(eachData?.price),
+                            setEditDesc(eachData?.description)
+                        )
+
+                          }}>Update</button>
+
+
+
+                          </td>
+                        </tr>
+
+
+                        
+                     
+                      </tbody>
+                    </Table>
+
+
+                  </div>
+                ))
+
+                :
+                <Alert variant="danger" onClose={() => setShow(false)} >
+                <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                <p>
+                  There is nothing to show you. Your Product list is empty. You should add
+                  some product first.
+                 
+                </p>
+              </Alert>
+                    
+
+        
+
+              
+        }
+
+        <div className='modal-div'>
+          <Modal
+            show={show}
+            backdrop="static"
+            keyboard={false}
+          >
+          <Modal.Header>
+            <Modal.Title>Modal title</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <form onSubmit={updateProductHandler} >
+                <input type="text" defaultValue = {editName} name = "product"
+                  // onChange = {(e) =>{
+                  //   setEditedName(e.target.value)
+
+                  // }} 
+                />
+            
+
+      
+          
+                <input type="text" defaultValue = {editPrice} name = "price"
+                  // onChange = {(e) =>{
+                  //   setEditedPrice(e.target.value)
+      
+                  // }} 
+                />
+                <textarea name="description" id="" cols="80" rows="5" defaultValue={editDesc} 
+                  // onChange = {(e) =>{
+                  //   setEditedDesc(e.target.value)
+      
+                  // }} 
+                ></textarea>
+              <Button variant="primary" type='submit' >Save Changes</Button>
+
+            </form>
+
+
+           
+          </Modal.Body>
+
+          <Modal.Footer>
+
+          </Modal.Footer>
+        </Modal>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+
+     
+
+
+
+
+      
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ server.mjs
@@ -0,0 +1,153 @@
+import express from "express" 
+import path from 'path';
+import cors from 'cors';
+
+
+
+const app = express()
+
+const port = process.env.PORT || 3000
+app.use(cors());
+let products = []; // TODO: connect with mongodb instead
+
+app.use(express.json());
+app.post('/product', (req, res) => {
+
+  const body = req.body;
+  console.log(body)
+
+  if ( // validation
+      !body.name
+      && !body.price
+      && !body.description
+  ) {
+      res.status(400).send({
+          message: "required parameters missing",
+      });
+      return;
+  }
+
+
+
+    products.push({
+        id: new Date().getTime(),
+        names: req.body.names,
+        price: req.body.price,
+        description: req.body.description
+    });
+
+    res.send({
+        data:body,
+        message: "product added successfully"
+    });
+    
+})
+
+app.get('/products', (req, res) => {
+    res.send({
+        message: "got all products successfully",
+        data: products
+    })
+})
+app.get('/product/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    let isFound = false;
+    for (let i = 0; i < products.length; i++) {
+
+        if (products[i].id === id) {
+            res.send({
+                message: `get product by id: ${products[i].id} success`,
+                data: products[i]
+            });
+
+            isFound = true
+            break;
+        }
+    }
+    if (isFound === false) {
+        res.status(404)
+        res.send({
+            message: "product not found"
+        });
+    }
+    return;
+})
+
+app.delete('/product/:ids', (req, res) => {
+    const id = Number(req.params.ids);
+
+    let isFound = false;
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === id) {
+            products.splice(i, 1);
+            res.send({
+                message: "product deleted successfully",
+            });
+            isFound = true
+            break;
+        }
+    }
+    if (isFound === false) {
+        res.status(404)
+        res.send({
+            message: "delete fail: product not found"
+        });
+    }
+})
+
+app.put('/product/:editId', (req, res) => {
+
+    const body = req.body;
+    const id =  Number(req.params.editId);
+
+    if ( // validation
+        !body.names
+        && !body.price
+        && !body.description
+    ) {
+        res.status(400).send({
+            message: "required parameters missing"
+        });
+        return;
+    }
+
+    let isFound = false;
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === id) {
+
+            products[i].names = body.names;
+            products[i].price = body.price;
+            products[i].description = body.description;
+
+            res.send({
+                message: "product modified successfully"
+            });
+            isFound = true
+            break;
+        }
+    }
+    if (!isFound) {
+        res.status(404)
+        res.send({
+            message: "edit fail: product not found"
+        });
+    }
+    res.send({
+        message: "product added successfully"
+    });
+})
+
+
+
+
+
+
+const __dirname = path.resolve();
+app.use('/', express.static(path.join(__dirname, './product/build')))
+app.use('*', express.static(path.join(__dirname, './product/build')))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
